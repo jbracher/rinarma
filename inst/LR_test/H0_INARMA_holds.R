eb75b95fb165c3bb14ba5d44e2f4b7fc6f041310
Barbora Sobolova <barbora.sobolova@kit.edu>
2024-11-22 12:02:40 +0100

    Script and partial results for the LR-test for INARMA vs. INARMA with generalized thinning.

---
+++ inst/LR_test/H0_INARMA_holds.R
@@ -0,0 +1,172 @@
+library(devtools)
+load_all()
+
+# define true values for the three scenarios:
+vals_tau <- c(1, 1, 1)
+vals_beta <- c(0.5, 0.2, 0.1)
+vals_kappa <- c(0.5, 0.6, 0.8)
+vals_zeta <- c(0.2, 0.5, 0.8)
+
+vals_lgt <- c(250, 500, 1000) # lengths of simulated time series
+n_sim <- 1000 # number of simulation runs
+
+# Keep track of scenarios and lengths that are done
+# Needs to be changed manually, when selecting scenarios and lengths
+progress_log <- expand.grid(scenario = 1:3, lgt = vals_lgt, already_done = FALSE,
+                            continue = FALSE)
+
+sims_to_run <- 1:9
+sims_to_run <- sims_to_run[!(progress_log$already_done)]
+
+# Set a few different starting value in case the optimization does not converge
+start_transformed <- matrix(
+  c(rep(NA, 5),
+    1, 0, 2, 0, 0,
+    0.2, -2, 0, 1, 1,
+    0.2, -1, 3, -1, -1
+    ), nrow = 4, ncol = 5, byrow = TRUE)
+colnames(start_transformed) <- c("tau.Intercept", "logit_kappa", "logit_phi",
+                                 "log_mean_E1", "logit_zeta")
+
+for (i in sims_to_run) {
+
+  continue <- progress_log$continue[i] # should existing results be read (to take up after error)
+  s <- progress_log$scenario[i]
+  lgt <- progress_log$lgt[i]
+
+  # run simulation:
+  print(paste0("Started scenario ", s, " length ", lgt, "."))
+
+  # grab true parameter values:
+  tau <- vals_tau[s]
+  beta <- vals_beta[s]
+  kappa <- vals_kappa[s]
+
+  # initialize matrix to store results:
+  # get existing results if desired (if simulation somehow failed halfway)
+  if (continue) {
+    res <- read.csv(
+      paste0("inst/LR_test/Results/H0_INARMA_holds_results_s", s, "_", lgt,
+             ".csv")
+    )
+    res_null <- read.csv(
+      paste0("inst/LR_test/Results/H0_INARMA_holds_results_null_s", s, "_", lgt,
+             ".csv")
+    )
+    dat <- unname(read.csv(
+      paste0("inst/LR_test/Results/pois_sim_", tau, "_", beta, "_", kappa, "_", lgt,
+             ".csv"))
+    )
+    inds_to_run <- Position(res$kappa, f = is.na):n_sim
+  } else {
+    inds_to_run <- 1:n_sim
+    res <- data.frame(
+      kappa = rep(NA, n_sim),
+      zeta = rep(NA, n_sim),
+      beta = rep(NA, n_sim),
+      tau = rep(NA, n_sim),
+      kappa_se = rep(NA, n_sim),
+      zeta_se = rep(NA, n_sim),
+      beta_se = rep(NA, n_sim),
+      tau_se = rep(NA, n_sim),
+      max_lik = rep(NA, n_sim),
+      convergence = rep(NA, n_sim)
+    )
+    res_null <- data.frame(
+      kappa = rep(NA, n_sim),
+      beta = rep(NA, n_sim),
+      tau = rep(NA, n_sim),
+      kappa_se = rep(NA, n_sim),
+      beta_se = rep(NA, n_sim),
+      tau_se = rep(NA, n_sim),
+      max_lik = rep(NA, n_sim),
+      convergence = rep(NA, n_sim)
+    )
+    dat <- matrix(nrow = n_sim, ncol = lgt)
+  }
+
+  # Simulate
+  for (k in inds_to_run) {
+    # Generate the data
+    set.seed(k)
+    dat[k, ] <- sim_inarma(beta = beta, kappa = kappa, tau = tau, lgt = lgt,
+                           family = "Poisson", offspring = "binomial")$X
+
+    # Get the moment estimates
+    mom_ests <- fit_inarma_moments(as.numeric(dat[k, ]), family = "Poisson")
+    start_transformed[1, ] <- c(
+      log(mom_ests$coefficients["tau"]),
+      log(mom_ests$coefficients["kappa"] / (1 - mom_ests$coefficients["kappa"])),
+      log((1 - mom_ests$coefficients["beta"]) / mom_ests$coefficients["beta"]),
+      1.5,
+      0.2
+    )
+    start_transformed[is.na(start_transformed) | is.infinite(start_transformed)] <- 0
+
+    # Refit the null model until we get a non-problematic estimate
+    refit_iter <- 1
+    refit <- TRUE
+    while (refit & refit_iter <= 4) {
+
+      # Find the maximum likelihood estimates
+      op_null <- fit_inarma(
+        as.numeric(dat[k, ]),
+        family = "Poisson",
+        offspring = "binomial",
+        return_se = TRUE,
+        start = start_transformed[refit_iter, c("tau.Intercept", "logit_kappa",
+                                                "logit_phi", "log_mean_E1")]
+      )
+
+      # Increment the counters
+      refit <- op_null$optim$convergence != 0
+      refit_iter <- refit_iter + 1
+    }
+
+    print(paste0("Null refitted: ", refit_iter - 2, " times."))
+    # Fit the alternative model
+    op <- fit_inarma(as.numeric(dat[k, ]), family = "Poisson",
+                     start = c(op_null$coefficients_raw, logit_zeta = 0),
+                     offspring = "binomial-Poisson", return_se = TRUE,
+                     control_optim = list(maxit = 1000))
+
+    # Refit the alternative (big) model until we get a non-problematic estimate
+    refit_iter <- 1
+    refit <- op$optim$convergence != 0
+    while (refit & refit_iter <= 4) {
+
+      # Find the maximum likelihood estimates
+      op <- fit_inarma(as.numeric(dat[k, ]), family = "Poisson",
+                       start = start_transformed[refit_iter,],
+                       offspring = "binomial-Poisson", return_se = TRUE,
+                       control_optim = list(maxit = 1000))
+
+      # Increment the counters
+      refit <- op$optim$convergence != 0
+      refit_iter <- refit_iter + 1
+    }
+    print(paste0("Alternative refitted: ", refit_iter - 1, " times."))
+
+    res$max_lik[k] <- op$loglikelihood
+    res_null$max_lik[k] <- op_null$loglikelihood
+    res[k, c("tau", "beta", "kappa", "zeta")] <- op$coefficients[1:4]
+    res_null[k, c("tau", "beta", "kappa")] <- op_null$coefficients[1:3]
+    res[k, c("tau_se", "beta_se", "kappa_se", "zeta_se")] <- op$se[1:4]
+    res_null[k, c("tau_se", "beta_se", "kappa_se")] <- op_null$se[1:3]
+    res[k, "convergence"] <- op$optim$convergence
+    res_null[k, "convergence"] <- op_null$optim$convergence
+
+    # Store the results every 10 iterations
+    if (k %% 10 == 0) {
+      print(paste0("Finished iteration: ", k, "."))
+      write.csv(res, file = paste0("inst/LR_test/Results/H0_INARMA_holds_results_s", s, "_", lgt, ".csv"),
+                row.names = FALSE)
+      write.csv(res_null, file = paste0("inst/LR_test/Results/H0_INARMA_holds_results_null_s", s, "_", lgt, ".csv"),
+                row.names = FALSE)
+      write.csv(dat, file = paste0("inst/LR_test/Results/pois_sim_", tau, "_", beta, "_",
+                                   kappa, "_", lgt, ".csv"), row.names = FALSE)
+    }
+  }
+  progress_log$already_done[i] <- TRUE
+  print(paste0("Finished scenario ", s, " length ", lgt, " check the results."))
+}
---
+++ inst/LR_test/results_check.R
@@ -0,0 +1,98 @@
+library(tidyverse)
+library(tscount)
+library(ks)
+
+vals_tau <- c(1, 1, 1)
+vals_beta <- c(0.5, 0.2, 0.1)
+vals_kappa <- c(0.5, 0.6, 0.8)
+vals_zeta <- c(0.2, 0.5, 0.8)
+
+#########################################################
+## H0: zeta = 0 (binomial thinning only) vs. H1: zeta > 0
+## H0 holds
+#########################################################
+
+# Scenario 1 is done for length 500 & 1000, for length 500 iteration 511 failed
+# for some reason, I'll fix it later
+# Scenario 2 is done for length 250
+# Scenario 3 is done for length 250
+
+s <- 3  # Scenario number
+lgt <- 250  # Length
+
+res <- read.csv(paste0("inst/LR_test/Results/H0_INARMA_holds_results_s", s, "_", lgt, ".csv"))
+res_null <- read.csv(paste0("inst/LR_test/Results/H0_INARMA_holds_results_null_s", s, "_", lgt, ".csv"))
+
+# Check the point estimates
+mean(res$kappa, na.rm = TRUE) - vals_kappa[s]
+mean(res_null$kappa, na.rm = TRUE) - vals_kappa[s]
+mean(res$tau, na.rm = TRUE) - vals_tau[s]
+mean(res_null$tau, na.rm = TRUE) - vals_tau[s]
+mean(res$beta, na.rm = TRUE) - vals_beta[s]
+mean(res_null$beta, na.rm = TRUE) - vals_beta[s]
+
+# Calculate the LR statistic
+LR_stat <- -2 * (res_null$max_lik - res$max_lik)
+
+# Select only the convergent iterations
+convergent <- which(res$convergence == 0 & res_null$convergence == 0)
+length(convergent)
+LR_stat_corrected <- LR_stat[convergent]
+
+# Set the negative values of the LR statistic to zero
+LR_stat_corrected2 <- ifelse(LR_stat_corrected < 0, 0, LR_stat_corrected)
+
+# The 95% critical value of the mixture distribution
+critical <- qchisq(0.9, 1)
+
+# Display with ggplot
+step <- 0.1
+xx_gg <- seq(0, max(LR_stat_corrected2), by = step)  # breaks for the histogram
+yy_gg <- 0.5 * diff(pchisq(c(xx_gg, xx_gg[length(xx_gg)] + step), 1)) / step
+yy_gg[1] <- yy_gg[1] + 0.5 / step
+
+ggplot() +
+  geom_col(aes(x = xx_gg, y = yy_gg), alpha = 0.5, fill = "red",
+           width = 0.1, just = 0) +
+  geom_histogram(aes(x = LR_stat_corrected2, y = after_stat(density)), alpha = 0.5,
+                 breaks = xx_gg) +
+  geom_vline(xintercept = critical, linetype = "dashed") +
+  coord_cartesian(xlim = c(0, 5), ylim = c(0, 7.5)) +
+  labs(title = expression("H0:"~zeta~"= 0 (INARMA only) vs. H1:"~zeta~"> 0"),
+       subtitle = bquote(Scenario == .(s)~~~Length == .(lgt)~~~"H0 holds"), x = "LR test statistic", y = "density") +
+  theme_bw()
+
+# "Zoom in"
+ggplot() +
+  geom_col(aes(x = xx_gg, y = yy_gg), alpha = 0.5, fill = "red",
+           width = 0.1, just = 0) +
+  geom_histogram(aes(x = LR_stat_corrected2, y = after_stat(density)), alpha = 0.5,
+                 breaks = xx_gg) +
+  geom_vline(xintercept = critical, linetype = "dashed") +
+  coord_cartesian(xlim = c(0, 5), ylim = c(0, 1.5)) +
+  labs(title = expression("H0:"~zeta~"= 0 (INARMA only) vs. H1:"~zeta~"> 0"),
+       subtitle = bquote(Scenario == .(s)~~~Length == .(lgt)~~~"H0 holds"), x = "LR test statistic", y = "density") +
+  theme_bw()
+
+# Calculate the p-values
+p_val <- 1 - 0.5 * (ifelse(LR_stat_corrected2 > 0, 1, 0) + pchisq(LR_stat_corrected2, 1))
+
+# Display the histogram of p-values - THERE IS A SPIKE BEFORE 0.5
+hist(p_val, breaks = seq(0, 1, by = 0.05), ylim = c(0, 510))
+abline(h = 500, col = "red")
+
+# We have too many p-values between 0.49 and 0.5
+hist(p_val, breaks = seq(0, 1, by = 0.01), ylim = c(0, 510))
+abline(h = 500, col = "red")
+
+# Very small values of the LR-statistic
+summary(LR_stat_corrected2[p_val >= 0.49 & p_val < 0.5])
+
+# A lot of close to zero estimates of the additional parameter zeta
+summary(res$zeta[p_val >= 0.49 & p_val < 0.5])
+
+# Calculate the error of the first kind
+alpha <- sum(p_val < 0.05) / length(LR_stat_corrected2)
+# Alternative calculation (same value)
+# alpha <- sum(LR_stat_corrected2 > critical) / length(LR_stat_corrected2)
+print(paste("Type I error:", alpha * 100, "%"))
